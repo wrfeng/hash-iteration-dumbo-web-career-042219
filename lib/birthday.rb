cc1e64d60ed7b72680384752e9392f7a4779435a
wei feng <theweifeng@gmail.com>
2019-04-13 15:53:29 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -11,3 +11,10 @@ def happy_birthday(birthday_kids)
 end
 
 
+def age_appropriate_birthday(birthday_kids)
+  birthday_kids.each do |kids_name, age|
+    if birthday_kids[:age] <= 12
+      puts "Happy Birthday #{kids_name}! You are now #{age} years old!" ]
+    end
+  end
+end
